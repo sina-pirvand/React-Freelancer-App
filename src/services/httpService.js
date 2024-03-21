6cfca72baf1ede7370bd806d0fc9ac7c34c7d54b
sina-prvnd <96621439+sina-prvnd@users.noreply.github.com>
2024-03-21 10:50:00 +0330

    ref and acc token

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -7,6 +7,29 @@ const app = axios.create({
   withCredentials: true,
 });
 
+app.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+app.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config;
+    if (err.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+      try {
+        const { data } = await axios.get(`${Base_URL}/user/refresh-token`, {
+          withCredentials: true,
+        });
+        if (data) return app(originalConfig);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    Promise.reject(err);
+  }
+);
+
 const http = {
   get: app.get,
   post: app.post,
