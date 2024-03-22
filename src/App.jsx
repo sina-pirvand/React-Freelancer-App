d6af0b689ec2c4e990a9d0a99814425ae22080d8
sina-prvnd <96621439+sina-prvnd@users.noreply.github.com>
2024-03-22 11:31:27 +0330

    sidebar ui and Navlinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import "./App.css";
@@ -7,7 +7,9 @@ import CompleteProfile from "./pages/CompleteProfile";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import AppLayout from "./common/AppLayout";
-import Owner from "./pages/Owner";
+import OwnerDashboard from "./pages/OwnerDashboard";
+import Projects from "./pages/Projects";
+import Project from "./pages/Project";
 
 function App() {
   const queryClient = new QueryClient();
@@ -18,8 +20,11 @@ function App() {
       <Routes>
         <Route path="/auth" element={<Auth />} />
         <Route path="/complete-profile" element={<CompleteProfile />} />
-        <Route element={<AppLayout />}>
-          <Route path="/owner" element={<Owner />} />
+        <Route path="/owner" element={<AppLayout />}>
+          <Route index element={<Navigate to="dashboard" replace />} />
+          <Route path="dashboard" element={<OwnerDashboard />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="project/:id" element={<Project />} />
         </Route>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
--- src/common/Sidebar/index.jsx
+++ src/common/Sidebar/index.jsx
@@ -1,5 +1,40 @@
+import { NavLink } from "react-router-dom";
+import { RxDashboard } from "react-icons/rx";
+import { HiOutlineCollection } from "react-icons/hi";
+
 const Sidebar = () => {
-  return <div className="p-4 border-l border-secondary-100">Sidebar</div>;
+  return (
+    <div className="px-4 py-8 border-l border-secondary-100">
+      <ul className="flex flex-col gap-4">
+        <li>
+          <NavLink
+            to="/owner/dashboard"
+            className={({ isActive }) =>
+              isActive
+                ? "sidebar-item bg-primary-100/60 text-primary-900"
+                : "sidebar-item"
+            }
+          >
+            <RxDashboard className="w-6 h-6" />
+            <span>داشبورد</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/owner/projects"
+            className={({ isActive }) =>
+              isActive
+                ? "sidebar-item bg-primary-100/60 text-primary-900"
+                : "sidebar-item"
+            }
+          >
+            <HiOutlineCollection className="w-6 h-6" />
+            <span>پروژه ها</span>
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
